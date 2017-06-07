6d7f46cc7230a7ecc3bfa0946c1729ce545c9ca2
Michael Avnyin <mavnyin88@gmail.com>
2017-06-06 21:47:01 -0400

    new nav

--- js/main.js
+++ js/main.js
@@ -1,35 +1,6 @@
-// /* Slideshow */
-// var slideIndex = 0;
-// startSlideShow();
+	var randomNumber = Math.floor((Math.random() * 7) + 1);
 
-// function startSlideShow(){
-// 	var i;
-// 	// var slides = document.querySelectorAll(".hero-image");
-// 	var slides = document.getElementsByClassName("hero-image");
-// 	for(i=0;i<slides.length;i++){
-// 		console.log(slides[i]);
-// 		slides[i].style.display = "none";
-// 	}
-// 	slideIndex++
-// 	if(slideIndex > slides.length){
-// 		slideIndex = 1;
-// 	}
-// 	slides[slideIndex-1].style.display = "block";
-// 	setTimeout(startSlideShow,2500);
-// }
-startSlideShow();
-var counter = 1;
+	document.getElementsByTagName("body")[0].setAttribute("id","hero-img-"+randomNumber);
 
-function startSlideShow(){
-
-	if (counter > 7){
-		counter = 1;
-	}
-	else{
-	// document.getElementsByTagName("body")[0].removeAttribute("id","");
-	document.getElementsByTagName("body")[0].setAttribute("id","hero-img-"+(counter++));
-	}
-	setTimeout(startSlideShow,3000);
-}
 
 
